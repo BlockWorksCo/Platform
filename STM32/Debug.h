4bea82d599260fe485a4569797be6c21ffd1dabf
Steve Tickle <Steve@BlockWorks.co>
2014-03-08 10:54:58 +0000

    - Builds for MSP.

---
+++ STM32/Debug.h
@@ -0,0 +1,15 @@
+
+
+
+#ifndef __STM32DEBUG_H__
+#define __STM32DEBUG_H__
+
+
+
+
+#define DEBUG_TEXT(msg)      
+
+
+
+#endif
+
---
+++ Win32/Debug.h
@@ -0,0 +1,15 @@
+
+
+
+#ifndef __WIN32DEBUG_H__
+#define __WIN32DEBUG_H__
+
+
+
+
+#define DEBUG_TEXT(msg)      printf("%s:%d %s\n", __FILE__, __LINE__, msg)
+
+
+
+#endif
+
