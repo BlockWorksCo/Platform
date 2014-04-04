911442ce50af741d5e721c404342f146e9324f94
Steve Tickle <Steve@BlockWorks.co>
2014-04-04 21:54:39 +0100

    - Added missing Utilities.h

---
+++ Common/Utilities.h
@@ -0,0 +1,20 @@
+
+
+
+#ifndef __UTILITIES_H__
+#define __UTILITIES_H__
+
+
+
+
+
+#define NUMBER_OF_ELEMENTS(array)       (sizeof(array)/sizeof(array[0]))
+
+#define DEBUGME                         while(true)
+
+
+
+#endif
+
+
+
