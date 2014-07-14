4629ee177bbd64add5da996371054ff898374d4d
Steve Tickle <Steve@ZooKoo.com>
2014-07-14 22:40:34 +0100

    - Linux UART code almost there.

--- Linux/UART.h
+++ Linux/UART.h
@@ -4,89 +4,136 @@
 #define __UART_H__
 
 
+#include <stdio.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <termios.h>
+#include <unistd.h>
 
 
 
 //
 //
 //
-template <	uint32_t Port,
-			typename TxQueueType, 
-			typename RxQueueType,
-			uint32_t BaudRate>
+template <  typename TxQueueType, 
+            typename RxQueueType,
+            uint32_t BaudRate>
 class UART
 {
+
 public:
 
-	//
-	// Initialise the queue.
-	//
-	UART(TxQueueType& _txQueue, RxQueueType& _rxQueue) :
-		txQueue(_txQueue),
-		rxQueue(_rxQueue),
-		queueTooSmallFlag(false)
-	{
-		initialise();
-	}
+    //
+    // Initialise the queue.
+    //
+    UART(char* portName, TxQueueType& _txQueue, RxQueueType& _rxQueue) :
+        txQueue(_txQueue),
+        rxQueue(_rxQueue),
+        queueTooSmallFlag(false)
+    {
+        struct termios  options;
+
+        printf("hello world\n");
+
+        fd = open(portName, O_RDWR | O_NOCTTY | O_NDELAY);
+        if(fd == -1)
+        {
+            perror("open_port: Unable to open:");
+        }
+        else
+        {
+            fcntl(fd, F_SETFL, 0);
+            printf("hi again\n");
+        }
+
+        tcgetattr(fd, &options);
+
+        cfsetispeed(&options, B115200);
+        cfsetospeed(&options, B115200);
+        options.c_cflag |= (CLOCAL | CREAD);
+        options.c_cflag &= ~PARENB;
+        options.c_cflag &= ~CSTOPB;
+        options.c_cflag &= ~CSIZE;
+        options.c_cflag |= CS8;
+        options.c_cflag &= ~( ICANON | ECHO | ECHOE |ISIG );
+        options.c_iflag &= ~(IXON | IXOFF | IXANY );
+        options.c_oflag &= ~OPOST;
+
+        tcsetattr(fd, TCSANOW, &options);
+    }
 
 
 
-	//
-	// Process the master command queue.
-	//
-	bool ProcessQueue()
-	{
-		if(txQueue.IsEmpty() == false)
-		{
-			//
-			// Let the ISR empty the queue.
-			//
-			return true;
-		}
+    //
+    // Process the master command queue.
+    //
+    bool ProcessQueue()
+    {
+        if(txQueue.IsEmpty() == false)
+        {
+            char            buffer[10];
+            int             bytes;
+            char            c;
+            int             x;
+            char*           bufptr;
+            
+            //
+            // Let the ISR empty the queue.
+            //
+            write(fd, "ATZ\r",4);
+            printf(" wrote\n");
+            bufptr = buffer;
 
-		return false;
-	}
+            fcntl(fd, F_SETFL, FNDELAY);
+            bytes = read(fd, bufptr, sizeof(buffer));
+            printf("number of bytes read is %d\n", bytes);
+            perror ("read error:");
 
+            for (x = 0; x < 10 ; x++)
+            {
+                c = buffer[x];
+                printf("%d  ",c);
+            }
+            close(fd);
 
+            //puts(buffer[0]);
+            printf("\nshould of put something out \n");
 
+            return true;
+        }
 
-	//
-	//
-	//
-	void ISR(void)
-	{
-		// Rx buffer not empty.
-		//if( USART_GetITStatus(usart, USART_IT_RXNE) )
-		{
-
-			//
-			// Data waiting in the UART Rx buffer, get it and put it in the queue
-			// for the AppLevel to process.
-			//
-			char 	receivedByte = usart->DR;
-			rxQueue.Put(receivedByte, queueTooSmallFlag);
-		}
-	}
-
-private:	
-
-
-	/* This funcion initializes the USART1 peripheral
-	 * 
-	 * Arguments: baudrate --> the baudrate at which the USART is 
-	 * 						   supposed to operate
-	 */
-	void initialise()
-	{
-	}
+        return false;
+    }
 
 
-	//
-	//
-	//
-	bool 			queueTooSmallFlag;
-	TxQueueType& 	txQueue;
-	RxQueueType& 	rxQueue;
+    //
+    //
+    //
+    void ISR(void)
+    {
+        // Rx buffer not empty.
+        //if( USART_GetITStatus(usart, USART_IT_RXNE) )
+        {
+
+            //
+            // Data waiting in the UART Rx buffer, get it and put it in the queue
+            // for the AppLevel to process.
+            //
+            char    receivedByte = 0;
+            rxQueue.Put(receivedByte, queueTooSmallFlag);
+        }
+    }
+
+private:    
+
+    //
+    //
+    //
+    bool            queueTooSmallFlag;
+    TxQueueType&    txQueue;
+    RxQueueType&    rxQueue;
+    int             fd;
 
 };
 
